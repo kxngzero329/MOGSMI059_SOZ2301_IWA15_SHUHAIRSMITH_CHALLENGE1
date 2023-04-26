ca920d906cf60d757f13299a4880f40651253e3c
SMITH MOGAMAT SHUHAIR <126153069+kxngzero329@users.noreply.github.com>
2023-04-26 13:54:51 +0200

    Add files via upload

---
+++ iwa15/challenge 1/java/javascript.js
@@ -0,0 +1,52 @@
+// scripts.js
+
+const data = {
+    lists: [
+      ['first', [15, 11, 13, 7, 5]],
+      ['second', [2, 6, 8, 4, 14, 12, 10]],
+      ['third', [9, 3, 1]],
+    ]
+  }
+  
+  // Only edit below
+  
+  const first = data.lists[0][1]
+  const second = data.lists[1][1]
+  const third = data.lists[2][1]
+  
+  const result = []
+  
+  const extractBiggest = () => {
+    if (first.slice(-1)[0] > second.slice(-1)[0] && first.slice(-1)[0] > third.slice(-1)[0]) {
+      return first.pop()
+    }
+  
+    if (second.slice(-1)[0] > third.slice(-1)[0]) {
+      return second.pop()
+    }
+    
+    return third.pop()
+  }
+  
+  // Only edit above
+  
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  result.push(extractBiggest())
+  
+  console.log(result)
+  
